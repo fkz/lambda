63c53b6a8f5786de76cfaee8c08ab4eb75a6c8d4
Fabian Schmitthenner <development@schmitthenner.eu>
2024-02-29 21:35:22 +0000

    Add options to switch between interactive / non-interactive programs

---
+++ src/human_readable.rs
@@ -0,0 +1,94 @@
+// Compile (more) human-readable files
+
+use crate::program::Program;
+use std::collections::HashMap;
+
+struct State {
+    chunks: Vec<(String, Program)>,
+    main: Option<Program>,
+    error: Option<(String, CompileError)>,
+    location: String
+}
+
+#[derive(Debug)]
+enum CompileError {
+    InvalidCode(&'static str),
+    FreeVariables(u8),
+    DuplicateName(String),
+}
+
+impl State {
+    fn add_error(&mut self, error: CompileError) {
+        self.error = Some((self.location.clone(), error));
+    }
+
+    fn verify(&mut self, program: &[u8]) {
+        match crate::program::verify(program) {
+            Err(e) => self.add_error(CompileError::InvalidCode(e)),
+            Ok(0) => (),
+            Ok(free) => self.add_error(CompileError::FreeVariables(free)),
+        }
+    }
+
+    fn compile(&self, code: &str) -> Program {
+        let mut c = String::from(code);
+        for (name, chunk) in self.chunks.iter().rev() {
+            c = c.replace(name, &hex::encode(chunk))
+        }
+        c.retain(|c| !c.is_whitespace());
+        hex::decode(c).unwrap().into_boxed_slice()
+    }
+
+    fn add_chunk(&mut self, name: &str, code: Program) {
+        self.verify(&code);
+        let index = match self.chunks.binary_search_by_key(&name.len(), |x| x.0.len()) {
+            Err(index) => index,
+            Ok(index) => index,
+        };
+
+        self.chunks.insert(index, (String::from(name), code));
+    }
+
+    fn add_main(&mut self, code: Program) {
+        self.verify(&code);
+        self.main = Some(code);
+    }
+
+    fn make() -> Self {
+        State {
+            chunks: Vec::new(),
+            main: None,
+            error: None,
+            location: "Start".to_string()
+        }
+    }
+
+    fn process_line(&mut self, line: &str) {
+      self.location = String::from(line);
+    }
+}
+
+pub fn parse_file(path: &str) -> Program {
+    let mut state = State::make();
+
+    for line in std::fs::read_to_string(path).unwrap().split('\n') {
+        if line.is_empty() { continue }
+        if line == "clear errors" { state.error = None; continue }
+        state.process_line(line);
+        if line.starts_with("//") {
+            continue;
+        }
+        let b: Box<[&str]> = line.split('=').collect();
+        match *b {
+            [code] => state.add_main(state.compile(code)),
+            [name, code] => state.add_chunk(name, state.compile(code)),
+            _ => panic!("Wrong program"),
+        }
+    }
+
+    if let Some(s) = state.error {
+      panic!("Error while parsing file: {:?}", s)
+    }
+
+    state.main.unwrap()
+}
--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,43 @@ mod interact;
 mod program;
 mod simple_env;
 
-use crate::interact::Environment;
+use program::{simplify, simplify_debug};
+use simple_env::Env;
+
+use crate::{interact::Environment, program::show};
 
 fn main() {
-    let path = std::env::args().nth(1).expect("no path given");
+    let mut index = 1;
+    let mut show_steps = false;
+    let mut interactive = None;
+    loop {
+        let flag = std::env::args().nth(index).expect("no path given");
+        if (!flag.starts_with("--")) {
+            break;
+        }
+        match flag.as_str() {
+            "--show-steps" => show_steps = true,
+            "--interactive" => interactive = Some(Env::make()),
+            other => panic!("Unknown flag {}", other)
+        }
+        index += 1;
+    }
+    
+    let path = std::env::args().nth(index).expect("no path given");
+
     let program = human_readable::parse_file(&path);
-    interact::interact(&mut simple_env::Env::make(), &program);
+
+    if let Some(mut env) = interactive {
+        interact::interact(&mut env, &program);
+    } else {
+        println!("Input: {}", show(&program));
+        let simplified = {
+            if show_steps {
+                simplify_debug(program)
+            } else {
+                simplify(program)
+            }
+        };
+        println!("Simplified: {}", show(&simplified));
+    }
 }
