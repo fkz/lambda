11e443fbde35d5edd8aed4316e4896bdc24794b5
Fabian Schmitthenner <development@schmitthenner.eu>
2024-02-29 22:57:54 +0000

    make command line interface more flexible
    Add possibility to do something like
    
    lambda_calculus --number examples/add 5 7

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,39 @@ mod human_readable;
 mod interact;
 mod program;
 mod simple_env;
+mod pretty;
 
-use program::{simplify, simplify_debug};
+use pretty::Pretty;
+use program::{Program, simplify, simplify_debug};
 use simple_env::Env;
 
 use crate::{interact::Environment, program::show};
 
+fn with_applications(p: Program, applications: Vec<Program>) -> Program {
+    if applications.is_empty() {
+        return p;
+    }
+    let mut result = Vec::new();
+    let mut count = applications.len();
+    while count > 6 {
+        count -= 6;
+        result.push(0xFF);
+    }
+    result.push(0x80 | ((1 << (count + 1)) - 1));
+    result.extend_from_slice(&p);
+    for application in applications {
+        result.extend_from_slice(&application);
+    }
+    result.into_boxed_slice()
+}
+
 fn main() {
     let mut index = 1;
     let mut show_steps = false;
+    let mut show_program = true;
     let mut interactive = None;
+    let mut prettify = None;
+    let mut show_hex = false;
     loop {
         let flag = std::env::args().nth(index).expect("no path given");
         if (!flag.starts_with("--")) {
@@ -22,6 +45,9 @@ fn main() {
         match flag.as_str() {
             "--show-steps" => show_steps = true,
             "--interactive" => interactive = Some(Env::make()),
+            "--number" => { prettify = Some(pretty::Number); show_program = false; },
+            "--show-program" => show_program = true,
+            "--show-hex" => show_hex = true,
             other => panic!("Unknown flag {}", other)
         }
         index += 1;
@@ -29,12 +55,29 @@ fn main() {
     
     let path = std::env::args().nth(index).expect("no path given");
 
+    let remaining = std::env::args().len() - index - 1;
+    let mut applications = Vec::new();
+    if remaining > 0 {
+        let prettify = prettify.as_ref().expect("when using arguments, use --number to parse them");
+        while (applications.len() < remaining) {
+            let arg = std::env::args().nth(index + applications.len() + 1).unwrap();
+            let program = prettify.string_to_program(&arg)
+                .expect(format!("Expected {}, but got {}", prettify.name(), arg).as_str());
+            applications.push(program);
+        }
+    }
+
     let program = human_readable::parse_file(&path);
+    let program = with_applications(program, applications);
+
+    println!("Verify {:?}", program::verify(&program));
 
     if let Some(mut env) = interactive {
         interact::interact(&mut env, &program);
     } else {
-        println!("Input: {}", show(&program));
+        if show_program {
+            println!("Input: {}", show(&program));
+        }
         let simplified = {
             if show_steps {
                 simplify_debug(program)
@@ -42,6 +85,16 @@ fn main() {
                 simplify(program)
             }
         };
-        println!("Simplified: {}", show(&simplified));
+        if show_program {
+            println!("Simplified: {}", show(&simplified));
+        }
+        if show_hex {
+            println!("Simplified: {}", hex::encode(&simplified));
+        }
+        if let Some(p) = prettify {
+            let result = p.program_to_string(&simplified)
+                .expect(format!("Couldn't parse result of program as {}", p.name()).as_str());
+            println!("Result: {}", result);
+        }
     }
 }
---
+++ src/pretty.rs
@@ -0,0 +1,64 @@
+use super::program::Program;
+
+
+pub trait Pretty {
+  fn program_to_string(&self, p: &[u8]) -> Option<String>;
+  fn string_to_program(&self, s: &str) -> Option<Program>;
+  fn name(&self) -> String;
+}
+
+pub struct Number;
+
+impl Pretty for Number {
+  fn name(&self) -> String {
+      "number".to_string()
+  }
+
+  fn string_to_program(&self, s: &str) -> Option<Program> {
+    let number: u32 = s.parse().ok()?;
+    if number == 0 {
+        return Some(Box::new([0x84, 0x00]));
+    }
+
+    let mut result = Vec::with_capacity(2 * (number as usize) + 1);
+    result.push(0x8C);
+    result.push(0x01);
+    let mut index = number;
+    while index > 1 {
+        index -= 1;
+        result.push(0x83);
+        result.push(0x01);
+    }
+    result.push(0x00);
+    Some(result.into_boxed_slice())
+  }
+
+  fn program_to_string(&self, p: &[u8]) -> Option<String> {
+      let expected_number = {
+        if p.len() == 2 { 
+          0
+        } else if p.len() % 2 == 1 {
+          p.len() / 2
+        } else {
+          return None;
+        }
+      };
+
+      if expected_number == 0 && p != [0x84, 0x00] { return None; };
+      if expected_number > 0 && p[0..2] != [0x8C, 0x01] { return None; };
+      if expected_number > 0 && p.last() != Some(&0x00) { return None; }
+
+      for i in 2..(p.len()-1) {
+        if (i % 2 == 0 && p[i] != 0x83) || (i % 2 == 1 && p[i] != 0x01) {
+          return None;
+        }
+      }
+
+      Some(expected_number.to_string())
+  }
+
+  
+}
+
+
+
