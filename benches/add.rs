a992ec1634e185ef91e2a07522b28b91336d0d2f
Fabian Schmitthenner <development@schmitthenner.eu>
2024-03-02 15:30:36 +0000

    flamegraphs in the benchmarks

--- benches/add.rs
+++ benches/add.rs
@@ -1,5 +1,7 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, Criterion, profiler::Profiler};
 use lambda_calculus::{execute, parse_arguments, pretty, Program};
+use pprof::criterion::{Output, PProfProfiler};
+
 
 fn do_benchmark(c: &mut Criterion, name: &str, program: Program) {
     c.bench_function(name, |b| b.iter(|| execute(black_box(program.clone()), false, false, false, true)));
@@ -19,5 +21,9 @@ fn b(c: &mut Criterion) {
     benchmark_function(c, "sub", "examples/sub",vec![50, 25]);
 }
 
-criterion_group!(benches, b);
+criterion_group! {
+    name = benches; 
+    config = Criterion::default().with_profiler(PProfProfiler::new(100, Output::Flamegraph(None)));
+    targets = b
+}
 criterion_main!(benches);
