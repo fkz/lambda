0c6f7b2ccef25a7b731ece8c839af73f63e5fae3
Fabian Schmitthenner <development@schmitthenner.eu>
2024-03-13 22:08:12 +0000

    benchmark tests with new

--- benches/add.rs
+++ benches/add.rs
@@ -10,7 +10,7 @@ fn do_benchmark<W: Measurement>(
     c: &mut BenchmarkGroup<'_, W>,
     id: BenchmarkId,
     program: Program,
-    alloc: &mut lambda_calculus::gc_mem::alloc_impl::Allocator,
+    //alloc: &mut lambda_calculus::gc_mem::alloc_impl::Allocator,
 ) {
     let mut steps = 0;
     execute(
@@ -18,24 +18,24 @@ fn do_benchmark<W: Measurement>(
         false,
         false,
         false,
-        lambda_calculus::ExecutionModel::NewGcByValue,
+        lambda_calculus::ExecutionModel::DirectByValue,
         &mut steps,
-        Some(alloc),
+        None,
     );
     c.throughput(Throughput::Elements(steps));
 
     c.bench_with_input(id, &program, |b, p| {
         let mut step_count = 0;
         b.iter(|| {
-            alloc.collect_garbage(true, false);
+            //alloc.collect_garbage(true, false);
             execute(
                 black_box(p.clone()),
                 false,
                 false,
                 false,
-                lambda_calculus::ExecutionModel::NewGcByValue,
+                lambda_calculus::ExecutionModel::DirectByValue,
                 &mut step_count,
-                Some(alloc),
+                None,
             )
         })
     });
@@ -45,31 +45,31 @@ fn benchmark_function<W: Measurement>(
     c: &mut BenchmarkGroup<'_, W>,
     path: &str,
     arguments: Vec<u32>,
-    alloc: &mut lambda_calculus::gc_mem::alloc_impl::Allocator,
+    //alloc: &mut lambda_calculus::gc_mem::alloc_impl::Allocator,
 ) {
     let id = BenchmarkId::from_parameter(format!("{:5}, {:5}", arguments[0], arguments[1]));
 
     let program = parse_arguments(path, &pretty::number_u32(), arguments);
 
-    do_benchmark(c, id, program, alloc);
+    do_benchmark(c, id, program); //, alloc);
 }
 
 fn b<W: Measurement>(c: &mut Criterion<W>) {
     let values = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
-    let alloc = lambda_calculus::gc_mem::alloc_impl::get_allocator();
+    //let alloc = lambda_calculus::gc_mem::alloc_impl::get_allocator();
 
     let mut group = c.benchmark_group("small-add");
 
     for i in values.iter() {
         group.throughput(Throughput::Elements(2 * *i as u64));
-        benchmark_function(&mut group, "examples/add", vec![*i, *i], alloc);
+        benchmark_function(&mut group, "examples/add", vec![*i, *i]);//, alloc);
     }
     group.finish();
 
     let mut group = c.benchmark_group("small-sub");
     for i in values.iter().take(5) {
         group.throughput(Throughput::Elements(3 * *i as u64));
-        benchmark_function(&mut group, "examples/sub", vec![*i * 2, *i], alloc);
+        benchmark_function(&mut group, "examples/sub", vec![*i * 2, *i]);//, alloc);
     }
     group.finish();
 
@@ -81,7 +81,7 @@ fn b<W: Measurement>(c: &mut Criterion<W>) {
             &mut group,
             "examples/add",
             vec![*i * 1000, *i * 1000],
-            alloc,
+            //alloc,
         );
     }
     group.finish();
@@ -90,7 +90,7 @@ fn b<W: Measurement>(c: &mut Criterion<W>) {
     group.sample_size(10);
     for i in values.iter().take(3) {
         group.throughput(Throughput::Elements(3 * *i as u64));
-        benchmark_function(&mut group, "examples/sub", vec![*i * 4, *i * 2], alloc);
+        benchmark_function(&mut group, "examples/sub", vec![*i * 4, *i * 2]);//, alloc);
     }
     group.finish();
 }
